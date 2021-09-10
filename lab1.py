de73ba1a1d3aa321751a0aed017fffc167789006
AnneWang <wanglvyang@shinetechchina.com>
2021-09-10 18:20:28 +0800

    Update lab1.py
    final

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ import requests
 
 print(requests.__version__)
 url_for_g = requests.get('http://google.com/')
-#print(url_for_g.text)
+print(url_for_g)
 
 url = 'https://raw.githubusercontent.com/Lyuyang0130/CMPUT404Labs/main/lab1.py'
 
